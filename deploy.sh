363b079e8fc788973d2e6faa119ff5465a67208f
曾季暘(ShawnTseng) <shawntseng40@gmail.com>
2020-07-19 18:32:19 +0800

    update deploy method

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-npm run build
-cd build
-git init
-git add -A
-git commit -m 'deploy'
-# 部署到 https://github.com/ShawnTseng/react-protfolio.git 分支為 gh-pages
-git push -f https://github.com/ShawnTseng/react-protfolio.git master:gh-pages
