73678138020bc388e1b0f0c6fa2acfc72ebe79c4
曾季暘(ShawnTseng) <shawntseng40@gmail.com>
2020-07-19 18:29:57 +0800

    Updates

---
+++ precache-manifest.d12b3b25f6a4d257b5f1c45ee7e9bc38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252f47e6ae9c0b517a4f3f8757769f09",
+    "url": "/react-protfolio/index.html"
+  },
+  {
+    "revision": "e2f18ea059f8d3c1c237",
+    "url": "/react-protfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4fad556b4f2adc3f881e",
+    "url": "/react-protfolio/static/js/2.0f306e96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-protfolio/static/js/2.0f306e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f18ea059f8d3c1c237",
+    "url": "/react-protfolio/static/js/main.f558ddb9.chunk.js"
+  },
+  {
+    "revision": "13aa6a0ae94af500d24a",
+    "url": "/react-protfolio/static/js/runtime-main.f1033ca7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-protfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
