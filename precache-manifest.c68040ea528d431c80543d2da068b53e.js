9236ec99635348630cb675bcccf65b7af2ea511a
曾季暘(ShawnTseng) <shawntseng40@gmail.com>
2020-07-19 18:15:18 +0800

    deploy

---
+++ precache-manifest.c68040ea528d431c80543d2da068b53e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9514e5fb72db373a5df03a2b5d127a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72b71f5853b4d8c0946d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03a44b448da3700fe486",
+    "url": "/static/js/2.783d113a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.783d113a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b71f5853b4d8c0946d",
+    "url": "/static/js/main.e95677f3.chunk.js"
+  },
+  {
+    "revision": "585d13e9a5a306d4db9f",
+    "url": "/static/js/runtime-main.adb41a92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
