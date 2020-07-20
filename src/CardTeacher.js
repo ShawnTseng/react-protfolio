1ef896e76173b1c5aa8f570854ba139a41e44a55
曾季暘(ShawnTseng) <shawntseng40@gmail.com>
2020-07-20 08:01:24 +0800

    remove link

--- src/CardTeacher.js
+++ src/CardTeacher.js
@@ -3,13 +3,10 @@ import './Card.css';
 
 class CardTeacher extends Component {
     render() {
-        const { name, link } = this.props.data;
+        const { name } = this.props.data;
 
         return (
-            // eslint-disable-next-line
-            <a herf={link}>
-                <span>{name}</span>
-            </a>
+            <span>{name}</span>
         );
     }
 }
