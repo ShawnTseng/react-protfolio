2fc785ffb602fa396298bd75cbd78fc57093bfd3
曾季暘(ShawnTseng) <shawntseng40@gmail.com>
2020-07-25 11:59:56 +0800

    REFACTOR: arrange legacy code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import Layout from './Layout';
-import Card from './Card';
-import MyButton from './MyButton';
-import YourButton from './YourButton';
 
-import lesson from './lesson-react.json';
 
 function App() {
 
   return (
     <div>
       <Layout />
-      <Card data={lesson} />
-      <hr />
-      <MyButton>Hello</MyButton>
-      <YourButton>World</YourButton>
     </div>
   );
 }
--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import Card from './Card';
+import MyButton from './MyButton';
+import YourButton from './YourButton';
+
+import lesson from './lesson-react.json';
 
 class Layout extends Component {
     state = {
         tabIndex: 0,
-        tabList: [{ label: 'Card', id: 0 }, { label: 'Other', id: 1 }]
+        tabList: [{ label: 'Card', id: 0 }, { label: 'Styled Component Try', id: 1 }]
     };
 
     changeTab = tabId => {
@@ -14,10 +19,17 @@ class Layout extends Component {
     }
 
     render() {
-        const { tabList } = this.state;
+        const { tabIndex, tabList } = this.state;
         return (
             <div>
                 <Navbar tabList={tabList} changeTab={this.changeTab} />
+                {tabIndex === 0 && <Card data={lesson} />}
+                {tabIndex === 1 &&
+                    <>
+                        <MyButton>Hello</MyButton>
+                        <YourButton>World</YourButton>
+                    </>
+                }
             </div>
         );
     }
